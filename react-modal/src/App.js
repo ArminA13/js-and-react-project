7e4f3f4f22edacf0c2fcbb23d3190a4d1a81d5d9
Armine Muradyan <muradyan.armin13@gmail.com>
2024-02-03 15:57:20 +0400

    add openForm and openModal components

--- react-modal/src/App.js
+++ react-modal/src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import OpenForm from './component/openForm';
+import CustomizedDialogs from './component/openModal';
+
 
 function App() {
   return (
     <div className="App">
-    
+      <CustomizedDialogs title= "User Form">
+        <OpenForm/>
+      </CustomizedDialogs>
+      
     </div>
   );
 }
