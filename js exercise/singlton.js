6dd2e5e39e6b8b5a531675b7e31e6743df068c4e
Armine Muradyan <muradyan.armin13@gmail.com>
2024-02-03 16:46:03 +0400

    Add _groupData and getGroupedData methods

--- js exercise/singlton.js
+++ js exercise/singlton.js
@@ -1,7 +1,5 @@
 class DataFetcher {
-    constructor() {
-        
-    }
+
 
     static getInstace() {
         if (!DataFetcher.instance) {
@@ -10,9 +8,42 @@ class DataFetcher {
         return DataFetcher.instance;
     }
 
-    
+    _groupData(data, key) {
+        const groupedData = {};
+
+        data.forEach(item => {
+            const groupKey = item[key];
+            if (!groupedData[groupKey]) {
+                groupedData[groupKey] = [];
+            }
+            groupedData[groupKey].push(item);
+        });
+
+        return groupedData;
+    }
+
+    _fetchData(apiURL, key)  {
+        return fetch(apiURL)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Response was not ok");
+                }
+                return response.json();
+            })
+            .then(data => {
+                return this._groupData(data, key);
+            })
+            .catch(error => {
+                throw error;
+            });
+    }
+
+    getGroupedData(key) {
+        return this._fetchData("https://jsonplaceholder.typicode.com/posts", key);
+    }
 }
 
-
 const dataFetcher = DataFetcher.getInstace();
+dataFetcher.getGroupedData("userId").then(groupedData => console.log(groupedData));
+
 
